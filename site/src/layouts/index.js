1cd6caa8562bd7757e77df67aa9a701f139a6099
Shrayas Rajagopal <shrayasr@users.noreply.github.com>
2017-10-28 09:18:28 +0530

    s/hacker new today/hacker news today

--- site/src/layouts/index.js
+++ site/src/layouts/index.js
@@ -31,7 +31,7 @@ class Template extends React.Component {
             }}
             to={'/'}
           >
-          Hacker New Today
+          Hacker News Today
           </Link>
         </h1>
       )
@@ -52,7 +52,7 @@ class Template extends React.Component {
             }}
             to={'/'}
           >
-          Hacker New Today
+          Hacker News Today
           </Link>
         </h3>
       )
